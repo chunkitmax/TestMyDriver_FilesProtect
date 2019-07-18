3280dffcb94b0191ed42886376db46653b53ed95
chunkitmax <chunkitmax1125@gmail.com>
2019-07-19 00:12:35 +0800

    Update files

---
+++ TestMyDriver_FilesProtect/Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.TabPage4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TabControl1.Location = New System.Drawing.Point(0, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(453, 277)
+        Me.TabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed
+        Me.TabControl1.TabIndex = 2
+        Me.TabControl1.TabStop = False
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Button4)
+        Me.TabPage1.Controls.Add(Me.Button3)
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Controls.Add(Me.ListBox1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 21)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(445, 252)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "防刪設置"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(6, 93)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(105, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "移除防刪資料夾"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(6, 64)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(105, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "移除防刪文件"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(6, 35)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(105, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "增加防刪資料夾"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(105, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "增加防刪文件"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(117, 3)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(325, 246)
+        Me.ListBox1.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Button5)
+        Me.TabPage2.Controls.Add(Me.Button6)
+        Me.TabPage2.Controls.Add(Me.Button7)
+        Me.TabPage2.Controls.Add(Me.Button8)
+        Me.TabPage2.Controls.Add(Me.ListBox2)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 21)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(445, 214)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "防讀設置"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(6, 93)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(105, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "移除防讀資料夾"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(6, 64)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(105, 23)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "移除防讀文件"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(6, 35)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(105, 23)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "增加防讀資料夾"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(6, 6)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(105, 23)
+        Me.Button8.TabIndex = 6
+        Me.Button8.Text = "增加防讀文件"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 12
+        Me.ListBox2.Location = New System.Drawing.Point(117, 3)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(325, 208)
+        Me.ListBox2.TabIndex = 5
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.Button9)
+        Me.TabPage3.Controls.Add(Me.Button10)
+        Me.TabPage3.Controls.Add(Me.Button11)
+        Me.TabPage3.Controls.Add(Me.Button12)
+        Me.TabPage3.Controls.Add(Me.ListBox3)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 21)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(445, 214)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "防寫設置"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(6, 93)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(105, 23)
+        Me.Button9.TabIndex = 9
+        Me.Button9.Text = "移除防寫資料夾"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(6, 64)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(105, 23)
+        Me.Button10.TabIndex = 8
+        Me.Button10.Text = "移除防寫文件"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(6, 35)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(105, 23)
+        Me.Button11.TabIndex = 7
+        Me.Button11.Text = "增加防寫資料夾"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(6, 6)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(105, 23)
+        Me.Button12.TabIndex = 6
+        Me.Button12.Text = "增加防寫文件"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'ListBox3
+        '
+        Me.ListBox3.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 12
+        Me.ListBox3.Location = New System.Drawing.Point(120, 0)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(325, 214)
+        Me.ListBox3.TabIndex = 5
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Controls.Add(Me.GroupBox2)
+        Me.TabPage4.Controls.Add(Me.GroupBox1)
+        Me.TabPage4.Location = New System.Drawing.Point(4, 21)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(445, 214)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "自身設置"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 122)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(439, 92)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "自身啟動設置"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(439, 113)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "保護設置"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "FileProtect"
+        Me.NotifyIcon1.Visible = True
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.SelectedPath = "C:\Documents and Settings\LYC.LYC-2009V\桌面"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.AddExtension = False
+        Me.OpenFileDialog1.InitialDirectory = "C:\"
+        Me.OpenFileDialog1.Title = "選取文件"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 289)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "文件保護"
+        Me.TopMost = True
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage4.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
+    Friend WithEvents TabPage4 As System.Windows.Forms.TabPage
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ TestMyDriver_FilesProtect/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+Imports Microsoft.Win32.SafeHandles
+Imports FilesProtect_Dll.FileProtect
+
+Public Class Form1
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        SSDT_Hook_Off()
+        'MsgBox("Call StopSys, Return : " & StopSys("MyDriver"))
+        'MsgBox("Call UnInstallSys, Return : " & UnInstallSys("MyDriver"))
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Dim Path As String = "C:\DbgView\MyDriver.sys"
+        Dim Path As String = My.Application.Info.DirectoryPath & "\Resources\MyDriver.sys"
+        MsgBox(Path)
+        Dim InstallRet As Boolean = InstallSys("MyDriver", "MyDriver", Path)
+        Dim StartRet As Boolean = StartSys("MyDriver")
+        If InstallRet AndAlso StartRet Then SSDT_Hook_Init()
+        MsgBox("Turn SSDT_Hook On! Return : " & SSDT_Hook_On())
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim show As DialogResult = OpenFileDialog1.ShowDialog
+        If show = Windows.Forms.DialogResult.OK Then
+            Dim b = OpenFileDialog1.FileName
+            MsgBox(b.Substring(0, b.IndexOf(":") + 1))
+            MsgBox(b.Substring(b.IndexOf(":") + 1))
+            添加防刪文件(b.Substring(0, b.IndexOf(":") + 1), b.Substring(b.IndexOf(":") + 1))
+            ListBox1.Items.Add(b)
+            OpenFileDialog1.Reset()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim show As DialogResult = FolderBrowserDialog1.ShowDialog
+        If show = Windows.Forms.DialogResult.OK Then
+            Dim b = FolderBrowserDialog1.SelectedPath
+            MsgBox(b.Substring(0, b.IndexOf(":") + 1))
+            MsgBox(b.Substring(b.IndexOf(":") + 1))
+            添加防刪資料夾(b.Substring(0, b.IndexOf(":") + 1), b.Substring(b.IndexOf(":") + 1))
+            ListBox1.Items.Add(b)
+            FolderBrowserDialog1.Reset()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If ListBox1.SelectedIndex >= 0 Then
+            用文件路徑移除防刪文件(CType(ListBox1.SelectedItem, String).Substring(CType(ListBox1.SelectedItem, String).IndexOf(":") + 1))
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+        Else
+            MsgBox("沒有選取目標")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If ListBox1.SelectedIndex >= 0 Then
+            移除防刪資料夾(CType(ListBox1.SelectedItem, String).Substring(CType(ListBox1.SelectedItem, String).IndexOf(":") + 1))
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+        Else
+            MsgBox("沒有選取目標")
+        End If
+    End Sub
+
+End Class
